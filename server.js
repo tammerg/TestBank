65ca8241182820e649115dab1ba6cd965b9fa5cc
Tammer Galal <tammergalal20@gmail.com>
2016-05-03 13:30:15 -0400

    working on server.js file

--- server.js
+++ server.js
@@ -1,2 +1,20 @@
 var express = require('express');
 var app = express();
+var exphb = require('express-handlebars');
+var PORT = process.env.PORT || 8080;
+var path = require ('path');
+app.use(express.static(path.join(__dirname + '.../public')));
+app.set('views', __dirname + '/views');
+app.use('/scripts', express.static('public/scripts'));
+app.use('/css', express.static('public/css'));
+app.use('/images', express.static('public/images'));
+app.engine('handlebars', exphb({defaultLayout:'main'}));
+app.set('view engine', 'handlebars');
+
+app.get('/', function(req, res) {
+  res.render('index', {msg: req.query.msg});
+});
+
+app.listen(PORT, function(){
+  console.log("You are listening on:" , PORT)
+});
