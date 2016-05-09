2707c2cf0dd2ecbc93cb41319efcc9fc1c37e7c0
Tammer Galal <tammergalal20@gmail.com>
2016-05-08 21:08:44 -0400

    setup views/layouts folder, as well as main.handlebars and index

--- server.js
+++ server.js
@@ -3,16 +3,13 @@ var app = express();
 var exphb = require('express-handlebars');
 var PORT = process.env.PORT || 8080;
 var path = require ('path');
-app.use(express.static(path.join(__dirname + '.../public')));
-app.set('views', __dirname + '/views');
-app.use('/scripts', express.static('public/scripts'));
-app.use('/css', express.static('public/css'));
-app.use('/images', express.static('public/images'));
-app.engine('handlebars', exphb({defaultLayout:'main'}));
+
+app.use(express.static(__dirname + "/public"));
+app.engine('handlebars', exphb({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-app.get('/', function(req, res) {
-  res.render('index', {msg: req.query.msg});
+app.get('/', function(req, res){
+  res.render('index');
 });
 
 app.listen(PORT, function(){
